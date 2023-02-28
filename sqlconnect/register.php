c38f3d022011e38f8d8a54ad43ca0f95ad193805
Bikramjit Singh <bxsingh0@gmail.com>
2023-02-27 20:31:49 -0500

    Connected Unity to MySQL Database

---
+++ sqlconnect/register.php
@@ -0,0 +1,25 @@
+<?php 
+
+$con = mysqli_connect('localhost','root','root','FormidActiveSeating');
+
+//check for connection
+if(mysqli_connect_errno()){
+	echo "1";
+	exit();
+
+}
+
+$username = $_POST["username"];
+$passsword = $_POST["password"];
+$firstname = $_POST["firstname"];
+$lastname = $_POST["lastname"];
+$email = $_POST["email"];
+
+
+$insertuserquery = "INSERT INTO User (username,pass,firstName,lastName,email) VALUES ( '" . $username . "' , '" . $passsword . "' , '" . $firstname . " ', '" . $lastname . " ', '" . $email . "');";
+
+mysqli_query($con, $insertuserquery) or die("4: Insert user query failed");
+
+echo("0");
+
+?>
